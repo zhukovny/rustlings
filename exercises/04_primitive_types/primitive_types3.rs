b3f3b829cd9f4868832f8eafb14dc7a457ec05e7
zhukovny <cruvar.zhukov@yandex.ru>
2023-11-29 18:46:45 +0700

    complete with primitive types

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -5,10 +5,12 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let mut a = vec![];
+    for i in 0..120 {
+        a.push(i)
+    }
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
